f38216a919de04ea970eb51ba18e4f40157002c7
Laura Alarcón <lxalarcon0@misena.edu.co>
2023-11-22 09:41:19 -0500

    add keycloak dependecies

--- src/main/java/com/bidi/transactions/controller/TransactionController.java
+++ src/main/java/com/bidi/transactions/controller/TransactionController.java
@@ -8,10 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,13 +28,17 @@ public class TransactionController {
     private final GetTransactionService getTransactionService;
 
     @GetMapping("/{phoneNumber}")
-    public ResponseEntity<List<TransactionResponse>> getTransaction(@PathVariable String phoneNumber) {
+    public ResponseEntity<List<TransactionResponse>> getTransaction(
+            @PathVariable String phoneNumber,
+            @RequestHeader("Authorization") String token) {
         List<TransactionResponse> response = getTransactionService.getTransactions(phoneNumber);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/")
-    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody CreateTransactionRequest createTransactionRequest) throws Exception {
+    public ResponseEntity<TransactionResponse> createTransaction(
+            @RequestBody CreateTransactionRequest createTransactionRequest,
+            @RequestHeader("Authorization") String token) throws Exception {
         TransactionResponse response = createTransactionService.createTransaction(createTransactionRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
